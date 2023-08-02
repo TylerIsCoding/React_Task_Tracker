d1a618a6325794f0267e7d4004b9c37c4b47193b
Tyler McKenna <tmckenna1988@gmail.com>
2023-08-02 16:50:15 -0400

    Added full functionality but it's stored locally on the system with no backend

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 import { useState } from "react";
 
 
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: "Doctors Appointment",
-        day: "August 15th at 11:30 am",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "Prescription Refill",
-        day: "August 16th",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "Grocery Shopping",
-        day: "August 17th at 2:00 pm",
-        reminder: false,
-    },
-  ])
+  const [ showAddTask, setShowAddTask ] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task }
+    setTasks([...tasks, newTask ])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => 
+      task.id === id ? {...task, reminder: !task.reminder} : task
+    ))
+  }
+
   return <div className="container">
-    <Header />
-    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'No tasks'}
+    <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+    {showAddTask && <AddTask onAdd={addTask}/>}
+    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks'}
   </div>
 }
 
