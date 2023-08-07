57d2614cf2e5f391881bdd467ac961c7c80197af
Tyler McKenna <tmckenna1988@gmail.com>
2023-08-07 17:03:03 -0400

    Deployment successful

--- src/App.js
+++ src/App.js
@@ -3,43 +3,67 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { useState, useEffect } from "react";
 
-
-
 function App() {
-  const [ showAddTask, setShowAddTask ] = useState(false)
-  const [tasks, setTasks] = useState(() => {
-    const saved = localStorage.getItem("tasks");
-    const initialValue = JSON.parse([saved]);
-    return initialValue.sort((a, b) => Date.parse(a.day) - Date.parse(b.day)) || [];
-  })
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-  }, [tasks])
+    const [showAddTask, setShowAddTask] = useState(false);
+    const [tasks, setTasks] = useState(() => {
+        const saved = localStorage.getItem("tasks");
+        if (!saved) {
+            localStorage.setItem("tasks", JSON.stringify([]));
+        }
+        const initialValue = JSON.parse([saved]);
+        return (
+            initialValue.sort(
+                (a, b) => Date.parse(a.day) - Date.parse(b.day)
+            ) || []
+        );
+    });
+    useEffect(() => {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }, [tasks]);
 
-  // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1
-    const newTask = {id, ...task }
-    setTasks([...tasks, newTask ].sort((a, b) => Date.parse(a.day) - Date.parse(b.day)))
-  }
+    // Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1;
+        const newTask = { id, ...task };
+        setTasks(
+            [...tasks, newTask].sort(
+                (a, b) => Date.parse(a.day) - Date.parse(b.day)
+            )
+        );
+    };
 
-  // Delete Task
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    // Delete Task
+    const deleteTask = (id) => {
+        setTasks(tasks.filter((task) => task.id !== id));
+    };
 
-  // Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => 
-      task.id === id ? {...task, reminder: !task.reminder} : task
-    ))
-  }
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, reminder: !task.reminder } : task
+            )
+        );
+    };
 
-  return <div className="container">
-    <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-    {showAddTask && <AddTask onAdd={addTask}/>}
-    {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No tasks'}
-  </div>
+    return (
+        <div className="container">
+            <Header
+                onAdd={() => setShowAddTask(!showAddTask)}
+                showAdd={showAddTask}
+            />
+            {showAddTask && <AddTask onAdd={addTask} />}
+            {tasks.length > 0 ? (
+                <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                />
+            ) : (
+                "No tasks"
+            )}
+        </div>
+    );
 }
 
 export default App;
